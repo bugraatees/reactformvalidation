2c095ea487ae1814e9799f78e35d9d8aa83ea262
bugraatees <bugraatees@hotmail.com>
2023-12-01 19:35:50 +0300

    k1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email:'',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const [errors, setErrors] = useState({})
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData, [name] : value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const validationErrors = {}
+    if (!formData.username.trim()){
+      validationErrors.username = "username is required"
+    }
+
+    if (!formData.email.trim()){
+      validationErrors.email = "email is required"
+    }
+    if (!formData.email.trim()){
+      validationErrors.password = "password is required"
+    } else if (formData.password.length < 6){
+      validationErrors.password = "password should be at least 6 char"
+    }
+
+    if(formData.confirmPassword !== formData.password) {
+      validationErrors.confirmPassword = "password not matched"
+    }
+
+    setErrors(validationErrors)
+
+    if(Object.keys(validationErrors).length ===0) {
+      alert ("Form Submitted succesfully")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form id='formvalidation' onSubmit = {handleSubmit}>
+    <div id='form'>
+      <div >
+        <label id='text'>Username : </label>
+        <input 
+        type='text'
+        name='username'
+        placeholder='Username..'
+        autoComplete='off'
+        onChange={handleChange}
+        />
+        {errors.username && <span>{errors.username}</span>}
+      </div>
+      <div>
+        <label id='text'>Email : </label>
+        <input 
+        type='email'
+        name='email'
+        placeholder='example@gmail.com'
+        autoComplete='off'
+        onChange={handleChange}
+        />
+        {errors.email && <span>{errors.email}</span>}
+      </div>
+      <div>
+        <label id='text'>Password : </label>
+        <input 
+        type='password'
+        name='password'
+        placeholder='*********'
+        onChange={handleChange}
+        />
+        {errors.password && <span>{errors.password}</span>}
+      </div>
+      <div>
+        <label id='text'>Confirm Password : </label>
+        <input 
+        type='password'
+        name='confirmPassword'
+        placeholder='*********'
+        onChange={handleChange}
+        />
+        {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+      </div>
+        <button id='button' type='submit'>Submit</button>
     </div>
+    </form>
   );
 }
 
